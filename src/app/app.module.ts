9c69e373d7d94bde6a2a9012255b2a786f8bb812
Simone Antunes <sanntosti@gmail.com>
2021-04-02 23:43:45 -0300

    Criação do jogo da velha

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { JogoDaVelhaModule } from './jogo-da-velha';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, JogoDaVelhaModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
